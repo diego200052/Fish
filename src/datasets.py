158d5d0455592f5c0ae2e99306a4695ff27931ff
diego200052 <com1pac56@hotmail.com>
2022-12-28 23:16:21 -0600

    test

--- src/datasets.py
+++ src/datasets.py
@@ -26,12 +26,13 @@ class CustomDataset(Dataset):
         self.height = height
         self.width = width
         self.classes = classes
-        self.image_file_types = ['*.jpg', '*.jpeg', '*.png', '*.ppm']
+        self.image_file_types = ['*.jpg']
         self.all_image_paths = []
         
         # get all the image paths in sorted order
         for file_type in self.image_file_types:
             self.all_image_paths.extend(glob.glob(f"{self.images_path}/{file_type}"))
+        print(all_image_paths)
         self.all_annot_paths = glob.glob(f"{self.labels_path}/*.xml")
         # Remove all annotations and images when no object is present.
         self.read_and_clean()
