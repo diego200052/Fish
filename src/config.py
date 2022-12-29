d620c7dbb7260bc26cb7fa9934e7312f57374404
diego200052 <com1pac56@hotmail.com>
2022-12-28 20:53:22 -0600

    initial commit

---
+++ src/config.py
@@ -0,0 +1,28 @@
+import torch
+
+BATCH_SIZE = 16 # increase / decrease according to GPU memeory
+RESIZE_TO = 512 # resize the image for training and transforms
+NUM_EPOCHS = 200 # number of epochs to train for
+NUM_WORKERS = 4
+
+DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+  
+# Images and labels direcotry should be relative to train.py
+TRAIN_DIR_IMAGES = '../input/train'
+TRAIN_DIR_LABELS = '../input/train'
+VALID_DIR_IMAGES = '../input/test'
+VALID_DIR_LABELS = '../input/test'
+
+# classes: 0 index is reserved for background
+CLASSES = [
+    '__background__',
+    'pezGordo', 'pezAmarillo', 'pezFuego', 'pezNaranja', 'pezGuppy', 'pezGato'
+]
+
+NUM_CLASSES = len(CLASSES)
+
+# whether to visualize images after creating the data loaders
+VISUALIZE_TRANSFORMED_IMAGES = False
+
+# location to save model and plots
+OUT_DIR = '../outputs'
